849e8598a49d538518a05112d99cfcbbc79d212e
Catherinen29 <cat_nevin@hotmail.com>
2024-03-29 14:25:00 +0000

    Make navbar tabbable. Add keypress eventlistener for open and close navbar.

--- script.js
+++ script.js
@@ -5,16 +5,35 @@ let closeNavBtn = document.querySelector('.close-nav')
 let openNavBtn = document.querySelector('.open-nav')
 let mainContent = document.querySelector('.main')
 
+
+// Click events
 const closeNav = () => {
-    navbar.style.display = 'none';
-    openNavBtn.style.display = 'block'
+    openNavBtn.classList.remove('hide')
+    navbar.classList.add('hide')
 }
 closeNavBtn.addEventListener('click', closeNav) 
 // mainContent.addEventListener('click', closeNav)
 
 const openNav = () => {
-    navbar.style.display = 'block'
-    openNavBtn.style.display = 'none'
     // mainContent.style.opacity = '0.5'
+    navbar.classList.remove('hide')
+    openNavBtn.classList.add('hide')
 }
 openNavBtn.addEventListener('click', openNav)
+
+
+// Keyboard events
+// Press the enter key when the openNavBtn is in focus
+const handleKeyPressOpen = (event) => {
+    if (event.key === 'Enter') {
+        openNav()
+    }
+}
+openNavBtn.addEventListener('keypress', handleKeyPressOpen)
+
+const handleKeyPressClose = (event) => {
+    if (event.key === 'Enter') {
+        closeNav()
+    }
+}
+closeNavBtn.addEventListener('keypress', handleKeyPressClose)
