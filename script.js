3646e28123689fe57cd913167e9876dbfe5e9b13
Catherinen29 <cat_nevin@hotmail.com>
2024-04-02 10:10:59 +0100

    Change background when navbar is open.

--- script.js
+++ script.js
@@ -6,47 +6,49 @@ let openNavBtn = document.querySelector('.open-nav')
 let body = document.querySelector('body')
 let transparentBg = document.querySelector('.transparent-bg')
 
-let mainContent = document.querySelector('.main')
-
 let navIsOpen = false
 
 // Click events
 const closeNav = () => {
+    // Show 'openNav button
     openNavBtn.classList.remove('hide')
+    // Hide navbar
     navbar.classList.add('hide')
-    // body.classList.remove('nav__open')
+
+    // Change background of main content back to normal
     transparentBg.style.opacity = '0'
     transparentBg.style.display = 'none'
     navIsOpen = false
+
+    // If navbar is closed, remove the eventlistener that closes the navbar
+    transparentBg.removeEventListener('click', handleClickOutsideNavbar)
+
 }
-closeNavBtn.addEventListener('click', closeNav) 
-// mainContent.addEventListener('click', closeNav)
+closeNavBtn.addEventListener('click', closeNav)
 
 const openNav = () => {
+    // Hide openNav button
     navbar.classList.remove('hide')
+    // Show navbar
     openNavBtn.classList.add('hide')
-    // body.classList.add('nav__open')
+    // Change background of main content to blurred
     transparentBg.style.display = 'block'
     transparentBg.style.opacity = '0.95'
     navIsOpen = true
+
+    // If navbar is open, close navbar by clicking outside of the navbar
+    transparentBg.addEventListener('click', handleClickOutsideNavbar)
 }
 openNavBtn.addEventListener('click', openNav)
 
 
-// const handleClickOutsideNavbar = (e) => {
-//     if (navIsOpen && !navbar.contains(e.target)) {
-//         console.log('HIT')
-//     }
-// }
-
-// if (navIsOpen) {
-//     body.addEventListener('click', handleClickOutsideNavbar)
-// } else {
-//     body.removeEventListener('click', handleClickOutsideNavbar)
-    
-// }
-
-
+const handleClickOutsideNavbar = (e) => {
+    // If the navbar is open, return true
+    // If e.target is not on the menu, return true
+    if (navIsOpen && !navbar.contains(e.target)) {
+        closeNav()
+    }
+}
 
 
 // Keyboard events
